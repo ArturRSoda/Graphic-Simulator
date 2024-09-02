f03ca860d321a4c1c1b1604a8f49a4a328c01cf2
ArturRSoda <artursodar@gmail.com>
2024-09-02 12:33:49 -0300

    quase la

--- Trabalho-1/main.py
+++ Trabalho-1/main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 
+from objects import Object, Point, Line, WireFrame
+
 class Frame(tk.Frame):
     def __init__(self, parent, width: int, height: int):
         super().__init__(parent, width=width, height=height, borderwidth=3, relief="groove")
@@ -25,12 +27,9 @@ class NewObjWindow:
         self.point_tab               : Tab
         self.line_tab                : Tab
         self.wireframe_tab           : Tab
-        self.polygon_tab             : Tab
         self.color_opt_frame         : Frame
         self.wireframe_coord_list    : list[tuple[int, int]]
-        self.polygon_coord_list      : list[tuple[int, int]]
         self.wireframe_coord_listbox : tk.Listbox
-        self.polygon_coord_listbox   : tk.Listbox
         self.color_opt_var           : tk.StringVar
         self.obj_name_var            : tk.StringVar
         self.tab_width               : int
@@ -40,7 +39,6 @@ class NewObjWindow:
         self.line_start_coord_tuple  : tuple[tk.IntVar, tk.IntVar]
         self.line_end_coord_tuple    : tuple[tk.IntVar, tk.IntVar]
         self.wireframe_coord_tuple   : tuple[tk.IntVar, tk.IntVar]
-        self.polygon_coord_tuple     : tuple[tk.IntVar, tk.IntVar]
 
         self.add_name_obj_entry()
         self.add_color_buttons()
@@ -74,7 +72,6 @@ class NewObjWindow:
         self.add_point_tab()
         self.add_line_tab()
         self.add_wireframe_tab()
-        self.add_polygon_tab()
 
         self.tab_menu.place(x=10, y=105)
 
@@ -137,45 +134,18 @@ class NewObjWindow:
 
         self.tab_menu.add(self.wireframe_tab, text="WireFrame")
 
-    def add_polygon_tab(self):
-        self.polygon_tab = Tab(self.tab_menu, width=self.tab_width, height=self.tab_height)
-
-        self.polygon_coord_list = list()
-        self.polygon_coord_tuple = (tk.IntVar(), tk.IntVar())
-        ttk.Label(self.polygon_tab, text="Coordinates").place(x=10, y=10)
-        self.add_coord_frame(self.polygon_tab, 10, 30, self.polygon_coord_tuple)
-
-        ttk.Label(self.polygon_tab, text="Added Coordinates").place(x=200, y=10)
-        self.polygon_coord_listbox = tk.Listbox(self.polygon_tab, width=10, height=7)
-        self.polygon_coord_listbox.place(x=200, y=30)
-
-        tk.Button(self.polygon_tab, text="Add Coord", command=self.add_polygon_coord).place(x=10, y=90)
-        tk.Button(self.polygon_tab, text="Add Coord", command=self.del_polygon_coord).place(x=10, y=130)
-        tk.Button(self.polygon_tab, text="Add", command=self.add_polygon).place(x=80, y=self.tab_height-45)
-        tk.Button(self.polygon_tab, text="Cancel", command=self.cancel).place(x=210, y=self.tab_height-45)
-
-        self.tab_menu.add(self.polygon_tab, text="Polygon")
 
     def add_wireframe_coord(self):
         coord = (self.wireframe_coord_tuple[0].get(), self.wireframe_coord_tuple[1].get())
         self.wireframe_coord_list.append(coord)
         self.wireframe_coord_listbox.insert(tk.END, "(%d , %d)" % (coord[0], coord[1]))
 
-    def add_polygon_coord(self):
-        coord = (self.polygon_coord_tuple[0].get(), self.polygon_coord_tuple[1].get())
-        self.polygon_coord_list.append(coord)
-        self.polygon_coord_listbox.insert(tk.END, "(%d , %d)" % (coord[0], coord[1]))
 
     def del_wireframe_coord(self):
         id = self.wireframe_coord_listbox.curselection()[0]
         self.wireframe_coord_listbox.delete(id)
         self.wireframe_coord_list.pop(id)
 
-    def del_polygon_coord(self):
-        id = self.polygon_coord_listbox.curselection()[0]
-        self.polygon_coord_listbox.delete(id)
-        self.polygon_coord_list.pop(id)
-        self.app.destroy()
 
     def add_point(self):
         coord=(self.point_coord_tuple[0].get(), self.point_coord_tuple[1].get())
@@ -190,10 +160,6 @@ class NewObjWindow:
 
     def add_wireframe(self):
         self.parent.add_wireframe(self.obj_name_var.get(), self.color_opt_var.get(), self.wireframe_coord_list)
-        self.app.destroy()
-
-    def add_polygon(self):
-        self.parent.add_polygon(self.obj_name_var.get(), self.color_opt_var.get(), self.polygon_coord_list)
         self.app.destroy()
 
     def cancel(self):
@@ -213,32 +179,64 @@ class CGSystem():
         self.messageBox        : tk.Listbox
         self.objects_listbox   : tk.Listbox
         self.canvas            : tk.Canvas
-        self.pace_var          : tk.IntVar
-        self.pace_entry        : tk.Entry
-        self.viewport_width    : int
-        self.viewport_height   : int
+        self.offset_var          : tk.IntVar
+        self.offset_entry        : tk.Entry
 
-        self.objects_list = list()
+        self.Wcoord_min        : tuple[int, int]
+        self.Wcoord_max        : tuple[int, int]
+        self.VPcoord_min       : tuple[int, int]
+        self.VPcoord_max       : tuple[int, int]
+
+        self.Wcoord_min = (-100, -100)
+        self.Wcoord_max = (100, 100)
+
+        self.display_file = list()
 
         self.add_menu()
         self.add_viewport()
         self.add_messagesBox()
+
+        #linha x e y
+        self.display_file.append(Line("X", "black", [(-10000, 0), (10000, 0)]))
+        self.display_file.append(Line("Y", "black", [(0, -10000), (0, 10000)]))
+        self.update_viewport()
+
+    def transform_coordinates(self, coord: tuple[int, int]) -> tuple[float, float]:
+        Xw = coord[0]
+        Yw = coord[1]
+
+        Xwmin, Ywmin  = self.Wcoord_min
+        Xwmax, Ywmax = self.Wcoord_max
+
+        Xvpmin, Yvpmin = self.VPcoord_min
+        Xvpmax, Yvpmax = self.VPcoord_max
+
+        Xvp = ( (Xw - Xwmin) / (Xwmax - Xwmin) ) * (Xvpmax - Xvpmin)
+        Yvp = ( 1 - ((Yw - Ywmin) / (Ywmax - Ywmin)) ) * (Yvpmax - Yvpmin)
+
+
+        print("resultado: ", Xvp, Yvp)
+        return (Xvp, Yvp)
+
 
     def run(self):
         self.app.mainloop()
 
     def add_viewport(self):
         self.app.update()
-        width = self.app.winfo_width()-self.menu_frame.winfo_width()-20
-        self.viewport_frame = Frame(self.app, width, 500)
+
+        self.viewport_frame = Frame(self.app, self.app.winfo_width()-self.menu_frame.winfo_width()-20, 500)
         self.viewport_frame.place(x=self.menu_frame.winfo_width()+20, y=10)
 
         Label(self.viewport_frame, "Viewport", 10).place(x=10, y=10)
 
         self.app.update()
-        self.viewport_height = self.viewport_frame.winfo_height()-70
-        self.viewport_width = self.viewport_frame.winfo_width()-35
-        self.canvas = tk.Canvas(self.viewport_frame, width=self.viewport_width, height=self.viewport_height, bg="white", borderwidth=5, relief="groove")
+
+        # sei la
+        self.VPcoord_max = (self.viewport_frame.winfo_width()-35, self.viewport_frame.winfo_height()-70)
+        self.VPcoord_min = (0, 0)
+
+        self.canvas = tk.Canvas(self.viewport_frame, width=self.VPcoord_max[0], height=self.VPcoord_max[1], bg="white", borderwidth=5, relief="groove")
         self.canvas.place(x=10, y=30)
 
     def add_messagesBox(self):
@@ -281,8 +279,9 @@ class CGSystem():
         id = self.objects_listbox.curselection()[0]
 
         self.objects_listbox.delete(id)
-        obj = self.objects_list.pop(id)
-        self.canvas.delete(obj)
+        self.display_file.pop(id+2)
+
+        self.update_viewport()
 
         self.add_message("Object Deleted")
 
@@ -302,16 +301,42 @@ class CGSystem():
 
         tk.Button(self.window_menu_frame, text="Set Coord", command=self.set_window_coord).place(x=150, y=135)
 
-        Label(self.window_menu_frame, "Pace", 10).place(x=10, y=145)
-        self.pace_var = tk.IntVar()
-        self.pace_var.set(10)
-        self.pace_entry = tk.Entry(self.window_menu_frame, textvariable=self.pace_var, width=4)
-        self.pace_entry.place(x=50, y=140)
+        Label(self.window_menu_frame, "offset", 10).place(x=10, y=145)
+        self.offset_var = tk.IntVar()
+        self.offset_var.set(10)
+        self.offset_entry = tk.Entry(self.window_menu_frame, textvariable=self.offset_var, width=4)
+        self.offset_entry.place(x=50, y=140)
+
+
+    def update_viewport(self):
+        self.canvas.delete("all")
+
+        for obj in self.display_file:
+            self.draw_object(obj)
+
+
+    def draw_object(self, obj: Object):
+
+        transformed_coordinates = []
+        for coord in obj.coordinates:
+            transformed_coordinates.append(self.transform_coordinates(coord))
+
+        if (len(transformed_coordinates) == 1):
+            coord = transformed_coordinates[0]
+            self.canvas.create_oval(coord[0], coord[1], coord[0], coord[1], fill=obj.color, width=5)
+
+        else:
+            for i in range(len(transformed_coordinates)-1):
+                start_coord = transformed_coordinates[i]
+                end_coord = transformed_coordinates[i+1]
+
+                self.canvas.create_line(start_coord[0], start_coord[1], end_coord[0], end_coord[1], fill=obj.color, width=2)
+
 
     def add_point(self,name: str, color: str, coord: tuple[int, int]):
-        point = self.canvas.create_oval(coord[0], coord[1], coord[0], coord[1], fill=color, width=5)
+        point = Point(name, color, [coord])
 
-        self.objects_list.append(point)
+        self.display_file.append(point)
         self.objects_listbox.insert(tk.END, "%s [%s - Point]" % (name, color))
 
         self.add_message("    - Coord:  (%d, %d)" % coord)
@@ -319,10 +344,12 @@ class CGSystem():
         self.add_message("    - Name:  %s" % name)
         self.add_message("Point added:")
 
-    def add_line(self, name: str, color: str, start_coord: tuple[int, int], end_coord: tuple[int, int]):
-        line = self.canvas.create_line(start_coord[0], start_coord[1], end_coord[0], end_coord[1], fill=color, width=2)
+        self.update_viewport()
 
-        self.objects_list.append(line)
+    def add_line(self, name: str, color: str, start_coord: tuple[int, int], end_coord: tuple[int, int]):
+        line = Line(name, color, [start_coord, end_coord])
+
+        self.display_file.append(line)
         self.objects_listbox.insert(tk.END, "%s [%s - Line]" % (name, color))
 
         self.add_message("    - Coord:  (%d, %d) to (%d, %d)" % (start_coord[0], start_coord[1], end_coord[0], end_coord[1]))
@@ -330,49 +357,26 @@ class CGSystem():
         self.add_message("    - Name:  %s" % name)
         self.add_message("Line added:")
 
+        self.update_viewport()
+
     def add_wireframe(self, name: str, color: str, coord_list: list[tuple[int, int]]):
         if (len(coord_list) == 1):
             self.add_point(name, color, coord_list[0])
+            return
 
-        elif (len(coord_list) == 2):
-            self.add_line(name, color, coord_list[0], coord_list[1])
+        wf = WireFrame(name, color, coord_list)
 
-        else:
-            flat_list : list[int]
-            flat_list = list(sum(coord_list, ()))
-            wf = self.canvas.create_line(flat_list, fill=color, width=2)
+        self.display_file.append(wf)
+        self.objects_listbox.insert(tk.END, "%s [%s - Wireframe]" % (name, color))
 
-            self.objects_list.append(wf)
-            self.objects_listbox.insert(tk.END, "%s [%s - Wireframe]" % (name, color))
+        coords = ""
+        for v in coord_list: coords += "(%d, %d) " % v
+        self.add_message("    - Coord:  %s" % coords)
+        self.add_message("    - Color:  %s" % color)
+        self.add_message("    - Name:  %s" % name)
+        self.add_message("Wireframe added:")
 
-            coords = ""
-            for v in coord_list: coords += "(%d, %d) " % v
-            self.add_message("    - Coord:  %s" % coords)
-            self.add_message("    - Color:  %s" % color)
-            self.add_message("    - Name:  %s" % name)
-            self.add_message("Wireframe added:")
-
-    def add_polygon(self, name: str, color: str, coord_list: list[tuple[int, int]]):
-        if (len(coord_list) == 1):
-            self.add_point(name, color, coord_list[0])
-
-        elif (len(coord_list) == 2):
-            self.add_line(name, color, coord_list[0], coord_list[1])
-
-        else:
-            flat_list : list[int]
-            flat_list = list(sum(coord_list, ()))
-            plg = self.canvas.create_polygon(flat_list, fill=color, width=2)
-
-            self.objects_list.append(plg)
-            self.objects_listbox.insert(tk.END, "%s [%s - Polygon]" % (name, color))
-
-            coords = ""
-            for v in coord_list: coords += "(%d, %d) " % v
-            self.add_message("    - Coord:  %s" % coords)
-            self.add_message("    - Color:  %s" % color)
-            self.add_message("    - Name:  %s" % name)
-            self.add_message("Polygon added:")
+        self.update_viewport()
 
     def set_window_coord(self):
         app = tk.Toplevel()
@@ -398,28 +402,58 @@ class CGSystem():
         self.add_message("Window coordinates seted to (%d, %d)" % (coord[0], coord[1]))
 
     def move_window_up(self):
-        pace = self.pace_var.get()
-        self.add_message("window moved up by %d" % pace)
+        offset = self.offset_var.get()
+
+        self.Wcoord_min = (self.Wcoord_min[0], self.Wcoord_min[1]+offset)
+        self.Wcoord_max = (self.Wcoord_max[0], self.Wcoord_max[1]+offset)
+        self.update_viewport()
+        
+        self.add_message("window moved up by %d" % offset)
 
     def move_window_down(self):
-        pace = self.pace_var.get()
-        self.add_message("window moved down by %d" % pace)
+        offset = self.offset_var.get()
+
+        self.Wcoord_min = (self.Wcoord_min[0], self.Wcoord_min[1]-offset)
+        self.Wcoord_max = (self.Wcoord_max[0], self.Wcoord_max[1]-offset)
+        self.update_viewport()
+
+        self.add_message("window moved down by %d" % offset)
 
     def move_window_left(self):
-        pace = self.pace_var.get()
-        self.add_message("window moved left by %d" % pace)
+        offset = self.offset_var.get()
+
+        self.Wcoord_min = (self.Wcoord_min[0]-offset, self.Wcoord_min[1])
+        self.Wcoord_max = (self.Wcoord_max[0]-offset, self.Wcoord_max[1])
+        self.update_viewport()
+
+        self.add_message("window moved left by %d" % offset)
 
     def move_window_right(self):
-        pace = self.pace_var.get()
-        self.add_message("window moved right by %d" % pace)
+        offset = self.offset_var.get()
+
+        self.Wcoord_min = (self.Wcoord_min[0]+offset, self.Wcoord_min[1])
+        self.Wcoord_max = (self.Wcoord_max[0]+offset, self.Wcoord_max[1])
+        self.update_viewport()
+
+        self.add_message("window moved right by %d" % offset)
 
     def zoom_window_in(self):
-        pace = self.pace_var.get()
-        self.add_message("window zoomed in by %d" % pace)
+        offset = self.offset_var.get()
+
+        self.Wcoord_min = (self.Wcoord_min[0]+offset, self.Wcoord_min[1]+offset)
+        self.Wcoord_max = (self.Wcoord_max[0]-offset, self.Wcoord_max[1]-offset)
+        self.update_viewport()
+
+        self.add_message("window zoomed in by %d" % offset)
 
     def zoom_window_out(self):
-        pace = self.pace_var.get()
-        self.add_message("window zoomed out by %d" % pace)
+        offset = self.offset_var.get()
+
+        self.Wcoord_min = (self.Wcoord_min[0]-offset, self.Wcoord_min[1]-offset)
+        self.Wcoord_max = (self.Wcoord_max[0]+offset, self.Wcoord_max[1]+offset)
+        self.update_viewport()
+
+        self.add_message("window zoomed out by %d" % offset)
 
     def add_message(self, message: str):
         self.messageBox.insert(0, message)
---
+++ Trabalho-1/objects.py
@@ -0,0 +1,24 @@
+from abc import ABC
+
+class Object(ABC):
+    def __init__(self, name: str, color: str, type: str, coordinates: list[tuple[int, int]]):
+        self.name = name
+        self.color = color
+        self.type = type
+        self.coordinates = coordinates
+
+class Point(Object):
+    def __init__(self, name: str, color: str, coordinates: list[tuple[int, int]]):
+        super().__init__(name, color, "point", coordinates)
+
+
+class Line(Object):
+    def __init__(self, name: str, color: str, coordinates: list[tuple[int, int]]):
+        super().__init__(name, color, "line", coordinates)
+
+
+class WireFrame(Object):
+    def __init__(self, name: str, color: str, coordinates: list[tuple[int, int]]):
+        super().__init__(name, color, "wireframe", coordinates)
+
+
